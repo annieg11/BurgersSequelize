6d70f1e5777c630f2adc5c2fa3e8f6f76cb8b83a
aanchal <aanchal.g.gupta@gmail.com>
2016-11-14 15:34:06 -0500

    getting request and need to work on delete query

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,37 +1,67 @@
 /*
-Here is where you create all the functions that will do the routing for your app, and the logic of each route.
+Here is where we create all the functions that will do the routing for our app, and the logic of each route.
 */
 var express = require('express');
 var router = express.Router();
-var burger = require('../models/burger.js');
+var models = require('../models');
+var sequelizeConnection = models.sequelize;
 
+// create routes
 router.get('/', function (req, res) {
-  res.redirect('/burgers');
+  res.redirect('/index');
 });
 
-router.get('/burgers', function (req, res) {
-  // burger.all(function (data) {
-  //   var hbsObject = { burgers: data };
-  //   console.log(hbsObject);
-  //   res.render('index', hbsObject);
-  // });
-});
+router.get('/index', function (req, res) {
+  models.burgers.findAll({}).then(function(data){
 
+    var hbsObject = { burgers: data };
+   // console.log(data);
+    res.render('index', hbsObject);
+
+  })
+});
+// To create an instance of a new burger
 router.post('/burgers/create', function (req, res) {
-  // burger.create(['burger_name'], [req.body.bname], function () {
-  //   res.redirect('/burgers');
-  // });
+    // Sequelize Query to add new burger to database
+  models.burgers.create(
+    {
+      burger_name: req.body.bname,
+      devoured: false
+    }
+  ).then(function(data){
+    // After the burger is added to the database, refresh the page
+    res.redirect('/index');
+  });
+
 });
+// To update burgers on parameter id 
+router.post('/burgers/update/:id', function (req, res) {
+   // we are using aquery to find the burger will a selected id.
+  models.burgers.findOne({ where: {id: req.params.id} })
+  .then(function(id) {
 
-router.put('/burgers/update/:id', function (req, res) {
-  // var condition = 'id = ' + req.params.id;
+  // now update devoured to true
+    id.update({
+      devoured: true
+    })
+    // After the burger is updated to the database, refresh the page
+    .then(function(){
+     res.redirect('/index');
+    })
+  });
 
-  // console.log('condition', condition);
-
-  // burger.update({ 'devoured': req.body.devoured }, condition, function () {
-  //   res.redirect('/burgers');
-  // });
 });
+router.delete('/burgers/delete/:id', function (req, res) {
+   // we are using aquery to find the burger will a selected id.
+  models.burgers.destroy({
+   where: {
+    id: req.params.id
+  } 
+ })
+    // After the burger is updated to the database, refresh the page
+     res.redirect('/index');
+    })
+ 
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,10 +4,21 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
 
 var app = express();
-// add the burgers model and sync it.
-// Syncing the model will create a matching table in our MySQL db. 
-var burgers = require("./models")["burgers"]
-burgers.sync(); // creates a burgers table
+// and we bring in our models folder. This brings in the model's object, as defined in index.js
+var models = require('./models');
+
+// extract our sequelize connection from the models object, to avoid confusion
+var sequelizeConnection = models.sequelize;
+// We run this query so that we can drop our tables even though they have foreign keys
+sequelizeConnection.query('SET FOREIGN_KEY_CHECKS = 0')
+
+// make our tables
+// note: force:true drops the table if it already exists
+.then(function(){
+  return sequelizeConnection.sync({force:true})
+})
+
+
 
 //Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
@@ -25,12 +36,13 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 
 // Get routes
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+var router = require('./controllers/burgers_controller.js');
+app.use('/', router);
+
 
 
 // Starts the server to begin listening
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
   console.log('App listening on PORT ' + PORT);
 })
